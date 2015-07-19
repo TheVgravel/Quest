8d346879ac344fa96de883762451ad758147e99e
TheVgravel <gravelv@gmail.com>
2015-07-18 21:32:38 -0400

    added the runing code

--- quest/src/main/java/org/formix/thevgravel/quest/Bonhomme.java
+++ quest/src/main/java/org/formix/thevgravel/quest/Bonhomme.java
@@ -3,22 +3,30 @@ package org.formix.thevgravel.quest;
 import java.awt.Graphics;
 import java.awt.Image;
 import java.io.IOException;
-import java.security.PublicKey;
 
 import javax.imageio.ImageIO;
+import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class Bonhomme {
 
 	private static final int SPRITE_WIDTH = 32;
 	private static final int SPRITE_HEIGHT = 32;
 
+	private JPanel container;
 	private Image rightRunSprite;
 	private int x;
 	private int y;
+	private int spriteIndex;
 
-	public Bonhomme() {
+	private boolean running;
+
+	public Bonhomme(JPanel container) {
+		this.running = false;
+		this.container = container;
 		this.x = 0;
 		this.y = 0;
+		this.spriteIndex = 0;
 		try {
 			this.rightRunSprite = ImageIO.read(this.getClass().getResourceAsStream("courir-droite-sprite.png"));
 		} catch (IOException e) {
@@ -41,21 +49,62 @@ public class Bonhomme {
 	public void setX(int x) {
 		this.x = x;
 	}
-	
-	public int getWidth(){
+
+	public int getSpriteIndex() {
+		return this.spriteIndex;
+	}
+
+	public void setSpriteIndex(int spriteIndex) {
+		this.spriteIndex = spriteIndex;
+	}
+
+	public int getWidth() {
 		return SPRITE_WIDTH;
 	}
-	
-	public int getHeight(){
+
+	public int getHeight() {
 		return SPRITE_HEIGHT;
 	}
 
-	public void draw(Graphics g, int spriteIndex) {
-		g.drawImage(this.rightRunSprite, this.x, this.y, SPRITE_WIDTH * 16, SPRITE_HEIGHT * 4, null);
-//		int spriteX = spriteIndex * SPRITE_WIDTH;
-//		g.drawImage(this.rightRunSprite, 
-//					this.x, this.y, SPRITE_WIDTH - 1, SPRITE_HEIGHT - 1, 
-//					spriteX, 0, spriteX, SPRITE_HEIGHT - 1, null);
+	public JPanel getContainer() {
+		return container;
 	}
 
+	public boolean isRunning() {
+		return this.running;
+	}
+
+	public void draw(Graphics g) {
+		int spriteX = this.spriteIndex * SPRITE_WIDTH;
+		g.setColor(this.container.getBackground());
+		g.fillRect(this.x, this.y, this.x + SPRITE_WIDTH * 4, this.y + SPRITE_HEIGHT * 4);
+		g.drawImage(this.rightRunSprite, this.x, this.y, this.x + SPRITE_WIDTH * 4, this.y + SPRITE_HEIGHT * 4, spriteX,
+				0, spriteX + SPRITE_WIDTH - 1, SPRITE_HEIGHT - 1, null);
+	}
+
+	public void startRunning(final int fps) {
+		this.running = true;
+		Thread t = new Thread(new Runnable() {
+			public void run() {
+				while (running) {
+					SwingUtilities.invokeLater(new Runnable() {
+						public void run() {
+							try {
+								Thread.sleep(1 / fps * 1000);
+							} catch (InterruptedException e) {
+								e.printStackTrace();
+							}
+							spriteIndex = (spriteIndex + 1) % 4;
+							container.repaint(x, y, x + SPRITE_WIDTH * 4, y + SPRITE_HEIGHT * 4);
+						}
+					});
+				}
+			}
+		});
+		t.run();
+	}
+
+	public void stopRunning() {
+		this.running = false;
+	}
 }
--- quest/src/main/java/org/formix/thevgravel/quest/MyPanel.java
+++ quest/src/main/java/org/formix/thevgravel/quest/MyPanel.java
@@ -8,116 +8,46 @@ import java.awt.event.MouseEvent;
 
 import javax.swing.BorderFactory;
 import javax.swing.JPanel;
+import javax.swing.SwingUtilities;
 
 public class MyPanel extends JPanel {
 	
+	private static final long serialVersionUID = 4463844263560893625L;
+	
 	private Bonhomme bonhomme;
-	private int imageX = 50;
-	private int imageY = 50;
-	private int imageW = 20;
-	private int imageH = 20;
 
 	public MyPanel() {
-		this.bonhomme = new Bonhomme();
-		setBackground(Color.black);
+		this.bonhomme = new Bonhomme(this);
+		this.bonhomme.setX(50);
+		this.bonhomme.setY(25);
+		setBackground(Color.darkGray);
 		setBorder(BorderFactory.createLineBorder(Color.black));
-		addMouseListener(new MouseAdapter() {
-			public void mousePressed(MouseEvent e) {
-				moveImage(e.getX(), e.getY());
+		this.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				SwingUtilities.invokeLater(new Runnable() {
+					public void run() {
+						if (!bonhomme.isRunning()) {
+							setBackground(Color.red);
+							bonhomme.startRunning(4);
+						} else {
+							setBackground(Color.darkGray);
+							bonhomme.stopRunning();
+						}
+					}
+				});
 			}
 		});
-
-		addMouseMotionListener(new MouseAdapter() {
-			public void mouseDragged(MouseEvent e) {
-				moveImage(e.getX(), e.getY());
-			}
-		});
-
 	}
 
-	private void moveImage(int x, int y) {
-
-		// Current square state, stored as final variables
-		// to avoid repeat invocations of the same methods.
-		final int CURR_X = bonhomme.getX();
-		final int CURR_Y = bonhomme.getY();
-		final int CURR_W = bonhomme.getWidth();
-		final int CURR_H = bonhomme.getHeight();
-		final int OFFSET = 1;
-
-		if ((CURR_X != x) || (CURR_Y != y)) {
-
-			// The square is moving, repaint background
-			// over the old square location.
-			repaint(CURR_X, CURR_Y, CURR_W + OFFSET, CURR_H + OFFSET);
-
-			// Update coordinates.
-			bonhomme.setX(x);
-			bonhomme.setY(y);
-
-			// Repaint the square at the new location.
-			repaint(bonhomme.getX(), bonhomme.getY(), bonhomme.getWidth() + OFFSET, bonhomme.getHeight() + OFFSET);
-		}
-		int OFFSET1 = 1;
-		if ((imageX != x) || (imageY != y)) {
-			repaint(imageX, imageY, imageW + OFFSET1, imageH + OFFSET1);
-			imageX = x;
-			imageY = y;
-			repaint(imageX, imageY, imageW + OFFSET1, imageH + OFFSET1);
-		}
-	}
 
 	public Dimension getPreferredSize() {
-		return new Dimension(800, 200);
+		return new Dimension(600, 200);
 	}
 
 	public void paintComponent(Graphics g) {
-		// Let UI Delegate paint first, which
-		// includes background filling since
-		// this component is opaque.
 		super.paintComponent(g);
-
-		// Draw Text
-		g.drawString("This is my custom Panel!", 10, 20);
-		bonhomme.draw(g, 0);
-	}
-}
-
-class RedSquare {
-
-	private int xPos = 50;
-	private int yPos = 50;
-	private int width = 20;
-	private int height = 20;
-
-	public void setX(int xPos) {
-		this.xPos = xPos;
+		bonhomme.draw(g);
 	}
 
-	public int getX() {
-		return xPos;
-	}
-
-	public void setY(int yPos) {
-		this.yPos = yPos;
-	}
-
-	public int getY() {
-		return yPos;
-	}
-
-	public int getWidth() {
-		return width;
-	}
-
-	public int getHeight() {
-		return height;
-	}
-
-	public void paintSquare(Graphics g) {
-		g.setColor(Color.RED);
-		g.fillRect(xPos, yPos, width, height);
-		g.setColor(Color.BLACK);
-		g.drawRect(xPos, yPos, width, height);
-	}
 }
